1dd71ff6d95e2c3723474ee660590621eb87f425
GroTion <grotion0720@gmail.com>
2021-06-08 20:44:38 +0800

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Shaun Ku on 2021/6/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
